dcf5f874e4582e79b20bda943278a2734abcd792
Eric Echemane <eechemane29@gmail.com>
2022-08-28 16:44:52 +0800

    css CssBaseline added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
