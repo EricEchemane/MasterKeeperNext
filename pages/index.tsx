883bbf95980ccf0915082b1f6494626dacced151
Eric Echemane <eechemane29@gmail.com>
2022-08-28 22:38:41 +0800

    able to detect if user is registered or not

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { Box, Button, Container } from '@mui/material';
 import useThemeContext from 'contexts/theme';
+import connectToDatabase from 'db/connect-to-database';
 import { IUser } from 'entities/user.entity';
 import { GetServerSideProps } from 'next';
 import { getToken } from 'next-auth/jwt';
+import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -12,6 +14,12 @@ export default function Home(
 ) {
   const router = useRouter();
   const { toggleTheme } = useThemeContext();
+  useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace('/sign-in');
+    },
+  });
 
   return <>
     <Head> <title> Master Keeper </title> </Head>
@@ -26,10 +34,39 @@ export default function Home(
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const token = await getToken({ req: context.req });
 
-  console.log(token);
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false
+      }
+    };
+  }
+  const db = await connectToDatabase();
+  if (!db) {
+    return {
+      redirect: {
+        destination: '/500',
+        permanent: false
+      }
+    };
+  }
 
+  const { User } = db.models;
+
+  const user = await User.findOne({
+    email: token.email
+  });
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/sign-up',
+        permanent: false
+      }
+    };
+  }
 
   return {
-    props: {}
+    props: { user }
   };
 };
