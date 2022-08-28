52917db2f840e0a4804b9c8d1fd1306f6193c426
Eric Echemane <eechemane29@gmail.com>
2022-08-28 16:42:39 +0800

    title added in Home route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  );
+  return <>
+    <Head> <title> Master Keeper </title> </Head>
+  </>;
 }
