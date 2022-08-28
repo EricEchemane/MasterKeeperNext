33ffafa34b241793a4823f376a752be46cad43fa
Eric Echemane <eechemane29@gmail.com>
2022-08-28 19:34:34 +0800

    added nextAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import { Button } from '@mui/material';
-import useThemeContext from 'contexts/theme';
+import { Box, Button, Container } from '@mui/material';
 import Head from 'next/head';
 import React from 'react';
 
 export default function Home() {
-  const { toggleTheme } = useThemeContext();
 
   return <>
     <Head> <title> Master Keeper </title> </Head>
-    <Button onClick={toggleTheme}> Toggle theme </Button>
+    <Container>
+      <Box p={'2rem'}>
+        <Button> Toggle theme </Button>
+      </Box>
+    </Container>
   </>;
 }
