9d2045c4da1c653c00bec881fcd0092e00988651
Eric Echemane <eechemane29@gmail.com>
2022-08-29 21:40:41 +0800

    adjust element spacing

--- app/components/home/NoAccountStateDisplay.tsx
+++ app/components/home/NoAccountStateDisplay.tsx
@@ -13,7 +13,7 @@ export default function NoAccountStateDisplay(props: { onAdd: () => void; }) {
                 alt='user accounts'
                 src='/no-account.svg' />
             <Typography variant='h5' my='1rem' align='center'> No accounts stored yet </Typography>
-            <Typography mb='1rem' variant='body2' align='center' sx={{ maxWidth: '450px' }}>
+            <Typography mb='2rem' variant='body2' align='center' sx={{ maxWidth: '450px' }}>
                 {"MasterKeeper allows you to store your accounts' credentials in a secure way so you don't have to remember them all. They are ecnrypted with your master password."}
             </Typography>
             <Button onClick={props.onAdd} endIcon={<AddOutlined />}> Add Some </Button>
