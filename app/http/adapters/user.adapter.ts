c9daa62e422e9f191a924f30c7beb071ca7fe2f3
Eric Echemane <eechemane29@gmail.com>
2022-08-29 12:57:06 +0800

    fixed wrong url

--- app/http/adapters/user.adapter.ts
+++ app/http/adapters/user.adapter.ts
@@ -4,7 +4,7 @@ import useHttpAdapter from "http/useHttpAdapter";
 const UserAdapters = Object.freeze({
     SignIn: () => useHttpAdapter<
         { name: string; email: string; image: string; password: string; },
-        null>(new HttpAdapter("/api/user/signin", 'POST'))
+        null>(new HttpAdapter("/api/user/sign-in", 'POST'))
 });
 
 export default UserAdapters;
