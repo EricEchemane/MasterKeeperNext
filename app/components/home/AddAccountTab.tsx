1e9ced1c80fde37ee74bb5fd2523aa1c6862e58f
Eric Echemane <eechemane29@gmail.com>
2022-08-29 21:53:06 +0800

    add account elements completed

--- app/components/home/AddAccountTab.tsx
+++ app/components/home/AddAccountTab.tsx
@@ -1,7 +1,21 @@
+import { LoadingButton } from '@mui/lab';
+import { Container, Stack, TextField } from '@mui/material';
 import React from 'react';
 
 export default function AddAccountTab() {
-    return (
-        <div>AddAccountTab</div>
-    );
+    return <>
+        <Container maxWidth='xs'>
+            <Stack
+                py='3rem' px='1rem'
+                spacing={5}
+                alignItems={'stretch'}>
+
+                <TextField label='Account label' required />
+                <TextField label='Username' required />
+                <TextField label='Password' required />
+                <TextField label='Url or link to your account' />
+                <LoadingButton variant='contained'> save </LoadingButton>
+            </Stack>
+        </Container>
+    </>;
 }
