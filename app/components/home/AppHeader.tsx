082c9fe7799a82597d49634a1bdf04b9d2b4d280
Eric Echemane <eechemane29@gmail.com>
2022-08-29 17:31:52 +0800

    AppHeader created

--- app/components/home/AppHeader.tsx
+++ app/components/home/AppHeader.tsx
@@ -1,19 +1,23 @@
 import { IUser } from 'entities/user.entity';
 import { Avatar, Box, Container, Divider, Stack, Typography } from '@mui/material';
+import AppHeaderMenu from './AppHeaderMenu';
 
 export default function AppHeader({ user }: { user: IUser; }) {
     return <>
         <Container>
             <Box p={'1rem'}>
-                <Stack direction='row' spacing={2} alignItems='center'>
-                    <Avatar
-                        sx={{ width: 45, height: 45 }}
-                        alt={user.name}
-                        src={user.image} />
-                    <Stack>
-                        <Typography variant='h6'> {user.name} </Typography>
-                        <Typography variant='caption'> {user.email} </Typography>
+                <Stack direction='row' alignItems='center' justifyContent='space-between'>
+                    <Stack direction='row' spacing={2} alignItems='center'>
+                        <Avatar
+                            sx={{ width: 45, height: 45 }}
+                            alt={user.name}
+                            src={user.image} />
+                        <Stack>
+                            <Typography variant='h6'> {user.name} </Typography>
+                            <Typography variant='caption'> {user.email} </Typography>
+                        </Stack>
                     </Stack>
+                    <AppHeaderMenu />
                 </Stack>
             </Box>
         </Container>
---
+++ app/components/home/AppHeaderMenu.tsx
@@ -0,0 +1,64 @@
+import { DarkModeOutlined, LightModeOutlined, LogoutOutlined, MoreVertOutlined } from '@mui/icons-material';
+import { IconButton, ListItemIcon, ListItemText } from '@mui/material';
+import { signOut } from 'next-auth/react';
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import useThemeContext from 'contexts/theme';
+
+export default function AppHeaderMenu() {
+    const { theme, toggleTheme } = useThemeContext();
+    const isDarkMode = theme === 'dark';
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const changeTheme = () => {
+        toggleTheme();
+        handleClose();
+    };
+    const signUserOut = () => {
+        signOut();
+        handleClose();
+    };
+
+    return (
+        <div>
+            <IconButton
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                aria-label='menu'>
+                <MoreVertOutlined />
+            </IconButton>
+
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}>
+                <MenuItem onClick={changeTheme}>
+                    <ListItemIcon>
+                        {isDarkMode ? <LightModeOutlined fontSize="small" /> : <DarkModeOutlined fontSize="small" />}
+                    </ListItemIcon>
+                    <ListItemText> {isDarkMode ? 'Light theme' : 'Dark theme'} </ListItemText>
+                </MenuItem>
+                <MenuItem onClick={signUserOut}>
+                    <ListItemIcon>
+                        <LogoutOutlined />
+                    </ListItemIcon>
+                    <ListItemText> Sign out </ListItemText>
+                </MenuItem>
+            </Menu>
+        </div>
+    );
+}
