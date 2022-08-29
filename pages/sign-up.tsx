e1a125ddacd2431dd04de04303d0a9a054aa675e
Eric Echemane <eechemane29@gmail.com>
2022-08-29 11:00:54 +0800

    use useForm and detect if password matched

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,10 +1,24 @@
-import { Avatar, Container, Stack, TextField, Typography } from '@mui/material';
+import { Avatar, Button, Container, Stack, TextField, Typography } from '@mui/material';
+import useForm from 'hooks/useForm';
 import useRequireSession from 'hooks/useRequireSession';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function SignUp() {
     const session = useRequireSession('/sign-in');
+    const [passwordNotMatch, setPasswordNotMatch] = useState(false);
+    const { values, handleChange } = useForm({
+        masterPassword: '',
+        confirmMasterPassword: '',
+    });
+    const save = () => {
+        if (values.masterPassword !== values.confirmMasterPassword) {
+            setPasswordNotMatch(true);
+        }
+    };
+    useEffect(() => {
+        setPasswordNotMatch(false);
+    }, [values]);
 
     if (!session) return <Head> <title>Sign up</title> </Head>;
     return <>
@@ -47,15 +61,26 @@ export default function SignUp() {
                     }}
                 />
                 <TextField
-                    id="password"
+                    name="masterPassword"
                     label="Master password"
                     type="password"
+                    value={values.masterPassword}
+                    onChange={handleChange}
                 />
                 <TextField
-                    id="confirm-password"
+                    name="confirmMasterPassword"
                     label="Confirm master password"
                     type="password"
+                    value={values.confirmMasterPassword}
+                    error={passwordNotMatch}
+                    helperText={passwordNotMatch ? "Password don't match" : ''}
+                    onChange={handleChange}
                 />
+                <Button
+                    onClick={save}
+                    variant='contained'>
+                    save
+                </Button>
 
             </Stack>
         </Container>
