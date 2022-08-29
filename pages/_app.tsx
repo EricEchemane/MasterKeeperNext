fbd02ac3e1032c0571a4f4810bddc63952a8d1ff
Eric Echemane <eechemane29@gmail.com>
2022-08-29 14:22:24 +0800

    user the user provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app';
 import { ThemeContextProvider } from 'contexts/theme';
 import { GetServerSidePropsContext } from 'next';
 import { getCookie } from 'cookies-next';
+import { UserProvider } from 'contexts/user/user.context';
 
 export default function MyApp({ Component, pageProps, colorScheme }
   : AppProps & { colorScheme: 'light' | 'dark'; }) {
   return <>
     <ThemeContextProvider colorScheme={colorScheme}>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </ThemeContextProvider>
   </>;
 }
