4f715484b69eee6699a44ba5e2dc4191e9894bd3
Eric Echemane <eechemane29@gmail.com>
2022-08-29 11:07:03 +0800

    remove FindByEmail adapter

--- app/http/adapters/user.adapter.ts
+++ app/http/adapters/user.adapter.ts
@@ -2,9 +2,7 @@ import HttpAdapter from "http/base-adapter";
 import useHttpAdapter from "http/useHttpAdapter";
 
 const UserAdapters = Object.freeze({
-    FindByEmail: () => useHttpAdapter
-        <null, { email: string; }>
-        (new HttpAdapter('/api/user/:email', 'GET'))
+
 });
 
 export default UserAdapters;
