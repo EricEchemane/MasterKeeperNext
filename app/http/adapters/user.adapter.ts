8c14580211a8410e115da4c84b695b4c7fba03f8
Eric Echemane <eechemane29@gmail.com>
2022-08-29 21:57:10 +0800

    new UserAdapters.AddAccount

--- app/http/adapters/user.adapter.ts
+++ app/http/adapters/user.adapter.ts
@@ -3,8 +3,24 @@ import useHttpAdapter from "http/useHttpAdapter";
 
 const UserAdapters = Object.freeze({
     SignIn: () => useHttpAdapter<
-        { name: string; email: string; image: string; password: string; },
-        null>(new HttpAdapter("/api/user/sign-in", 'POST'))
+        {
+            name: string;
+            email: string;
+            image: string;
+            password: string;
+        },
+        null
+    >(new HttpAdapter("/api/user/sign-in", 'POST')),
+
+    AddAccount: () => useHttpAdapter<
+        {
+            accountLabel: string;
+            username: string;
+            password: string;
+            url: string;
+        },
+        null
+    >(new HttpAdapter("/api/user/add-account", 'POST'))
 });
 
 export default UserAdapters;
