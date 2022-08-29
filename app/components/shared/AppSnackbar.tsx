273867483fc850395e13466c4ea154db11353022
Eric Echemane <eechemane29@gmail.com>
2022-08-30 01:18:15 +0800

    useNotification

---
+++ app/components/shared/AppSnackbar.tsx
@@ -0,0 +1,26 @@
+import Snackbar from '@mui/material/Snackbar';
+import { Alert } from '@mui/material';
+import { severityType } from 'hooks/useNotification';
+
+export default function AppSnackbar(props: {
+    message: string;
+    severity: severityType;
+    open: boolean;
+    onClose: () => void;
+}) {
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        props.onClose();
+    };
+
+    return <>
+        <Snackbar open={props.open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                {props.message}
+            </Alert>
+        </Snackbar>
+    </>;
+}
