61d21cd40b66f7b54f3655ef253d0200d2e876c7
Eric Echemane <eechemane29@gmail.com>
2022-08-30 01:21:32 +0800

    set the user using dispatch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import AppHeader from 'components/home/AppHeader';
 import AppTabs from 'components/home/AppTabs';
+import useUserContext from 'contexts/user/user.context';
 import connectToDatabase from 'db/connect-to-database';
 import { IUser } from 'entities/user.entity';
 import { GetServerSideProps } from 'next';
 import { getToken } from 'next-auth/jwt';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function Home(
   { user }: { user: IUser; }
 ) {
+  const { dispatch } = useUserContext();
+  useEffect(() => {
+    dispatch({
+      type: 'set_user',
+      payload: user
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <>
     <Head> <title> Master Keeper </title> </Head>
