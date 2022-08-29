d4f655d859ae5ff12edb181c05a0c01457ab8881
Eric Echemane <eechemane29@gmail.com>
2022-08-29 22:47:43 +0800

    get password modal setup

--- app/components/home/AddAccountTab.tsx
+++ app/components/home/AddAccountTab.tsx
@@ -1,8 +1,22 @@
 import { LoadingButton } from '@mui/lab';
 import { Container, Stack, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import GetMasterPasswordModal from './GetMasterPasswordModal';
 
 export default function AddAccountTab() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const save = async (password: string) => {
+
+    };
+    const receivePassword = (password: string) => {
+        setModalIsOpen(false);
+        save(password);
+    };
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
     return <>
         <Container maxWidth='xs'>
             <Stack
@@ -14,8 +28,16 @@ export default function AddAccountTab() {
                 <TextField label='Username' required />
                 <TextField label='Password' required />
                 <TextField label='Url or link to your account' />
-                <LoadingButton variant='contained'> save </LoadingButton>
+                <LoadingButton
+                    onClick={openModal}
+                    variant='contained'>
+                    save
+                </LoadingButton>
             </Stack>
         </Container>
+        <GetMasterPasswordModal
+            open={modalIsOpen}
+            onClose={() => setModalIsOpen(false)}
+            onPasswordSubmitted={receivePassword} />
     </>;
 }
---
+++ app/components/home/GetMasterPasswordModal.tsx
@@ -0,0 +1,72 @@
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/system';
+import { TextField } from '@mui/material';
+import { useState } from 'react';
+import useForm from 'hooks/useForm';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function GetMasterPasswordModal(props: {
+    open: boolean;
+    onClose: () => void;
+    onPasswordSubmitted: (p: string) => void;
+}) {
+    const [hasError, setHasError] = useState(false);
+    const { values, handleChange } = useForm({ password: '' });
+    const _continue = () => {
+        if (values.password.toString().trim() === '') {
+            setHasError(true);
+            return;
+        }
+        props.onPasswordSubmitted(values.password.toString());
+        setHasError(false);
+        values.password = '';
+    };
+
+    return (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={props.open}
+            onClose={props.onClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Fade in={props.open}>
+                <Box sx={style}>
+                    <Stack spacing={3}>
+                        <Typography id="transition-modal-title" variant="h6">
+                            Please enter your master password
+                        </Typography>
+                        <TextField
+                            error={hasError}
+                            helperText={hasError && 'Please enter your master password'}
+                            name='password'
+                            value={values.password}
+                            onChange={handleChange}
+                            label='Master password' />
+                        <Button onClick={_continue}> continue </Button>
+                    </Stack>
+                </Box>
+            </Fade>
+        </Modal >
+    );
+}
