9dc974cd93d5aec75ee1bacf7c1042f90913f89f
Eric Echemane <eechemane29@gmail.com>
2022-08-29 10:37:36 +0800

    beautify sign-in page

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,8 +1,9 @@
 import { signIn, useSession } from "next-auth/react";
-import { Box, Button, Container } from '@mui/material';
+import { Box, Button, Container, Stack } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { Google } from "@mui/icons-material";
 
 export default function SignIn() {
     const { data: session } = useSession();
@@ -16,10 +17,16 @@ export default function SignIn() {
 
     return <>
         <Head> <title>Sign in</title> </Head>
-        <Container>
-            <Box>
-                <Button onClick={() => signIn('google')}> Continue with Google </Button>
-            </Box>
+        <Container maxWidth='xs'>
+            <Stack justifyContent='stretch' py='3rem'>
+                <Button
+                    size='large'
+                    endIcon={<Google />}
+                    variant="contained"
+                    onClick={() => signIn('google')}>
+                    Continue with Google
+                </Button>
+            </Stack>
         </Container>
     </>;
 }
