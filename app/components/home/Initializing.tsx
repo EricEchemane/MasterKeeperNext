37e60d0c00f019f50185b028f7d543e83fd14fed
Eric Echemane <eechemane29@gmail.com>
2022-08-30 10:25:29 +0800

    added Initializing component

---
+++ app/components/home/Initializing.tsx
@@ -0,0 +1,10 @@
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+
+export default function Initializing() {
+    return <>
+        <Stack alignItems='center' mt='2rem'>
+            <Typography variant='h3' align='center'> Master Keeper </Typography>
+        </Stack>
+    </>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
+import { Grow } from '@mui/material';
 import AppHeader from 'components/home/AppHeader';
 import AppTabs from 'components/home/AppTabs';
+import Initializing from 'components/home/Initializing';
 import useUserContext from 'contexts/user/user.context';
 import useRequireSession from 'hooks/useRequireSession';
 import UserAdapters from 'http/adapters/user.adapter';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function Home() {
   useRequireSession('/sign-in');
   const { dispatch, state: user } = useUserContext();
+  const router = useRouter();
   const getUser = UserAdapters.Get();
 
   useEffect(() => {
     getUser.execute({})
       .then(data => {
-        if (!data) return;
+        if (!data) {
+          router.replace('/sign-up');
+          return;
+        }
         dispatch({
           type: 'set_user',
           payload: data.data
@@ -23,10 +30,12 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (user.email === '') return <></>;
-  return <>
-    <Head> <title> Master Keeper </title> </Head>
-    <AppHeader user={user} />
-    <AppTabs />
-  </>;
+  if (user.email === '') return <Initializing />;
+  return <Grow in={user.email !== ''}>
+    <div>
+      <Head> <title> Master Keeper </title> </Head>
+      <AppHeader user={user} />
+      <AppTabs />
+    </div>
+  </Grow>;
 }
